19bf4e0cc74687cef1650e732b2c706dfd6b6930
nauman zari oasis infobyte <zarinauman8@gmail.com>
2022-11-12 19:23:52 +0530

    Delete Functionality

--- 01-birthday-reminder/setup/src/App.js
+++ 01-birthday-reminder/setup/src/App.js
@@ -1,8 +1,28 @@
-import React, { useState } from 'react';
-import data from './data';
-import List from './List';
+import React, { useState } from "react";
+import data from "./data";
+import List from "./List";
 function App() {
-  return <h2>reminder project setup</h2>;
+  const [people, setPeople] = useState(data);
+  const deletePerson = (e) => {
+    setPeople(people.filter(person => person.id != e));
+  }
+  return (
+    <>
+      <main>
+        <section className="container">
+          <h3>{people.length} Birthdays Today</h3>
+          <List people={people} deletePerson={deletePerson} />
+          {
+            people.length == 0
+            ?
+            <button onClick={() => setPeople(data)}>Bring Back All</button>
+            :
+            <button onClick={() => setPeople([])}>Clear All</button>
+          }
+        </section>
+      </main>
+    </>
+  );
 }
 
 export default App;
--- 01-birthday-reminder/setup/src/List.js
+++ 01-birthday-reminder/setup/src/List.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React from "react";
 
-const List = () => {
+const List = ({ people,deletePerson }) => {
   return (
     <>
-      <h2>list component</h2>
+      {people.map((person) => {
+        const { id, name, age, image } = person;
+        return (
+          <>
+            <article key={id} className="person">
+              <img src={image} alt={name} />
+              <div>
+                <h4>{name}</h4>
+                <div style={{display:'flex',justifyContent:'space-between'}}>
+                  <span>{age} years</span>
+                  <span>
+                    <button style={{ width: "80px",margin:'-10px' }} onClick={(e)=>deletePerson(e.target.value)} value={id}>Delete</button>
+                  </span>
+                </div>
+              </div>
+            </article>
+          </>
+        );
+      })}
     </>
   );
 };
